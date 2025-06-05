e99dec3e0df5c48e7e55dbdf7c5f71f0701df8e9
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-06-05 11:12:29 +0200

    commit

---
+++ router.js
@@ -0,0 +1,20 @@
+const routes = {
+    "/": "<h1>Dit is de home pagina!</h1>",
+    "/about": "<h1>Dit is de about pagina!</h1>",
+    "/contact": "<h1>Dit is de contact pagina!</h1>"
+}
+
+const handleRoute = () => {
+    const url = window.location.pathname;
+    const content = routes[url] || "<h1>ERROR - 404</h1>";
+    document.getElementById("root").innerHTML = content;
+}
+
+document.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = e.target.getAttribute("href");
+    history.pushState({}, "", url);
+    handleRoute();
+})
+
+window.addEventListener("popstate", handleRoute);
